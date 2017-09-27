0dbd198489b52d92a503c82641adb6d173ef2560
Sean McPherson <seanmcp25@gmail.com>
2017-09-27 12:05:49 -0400

    Set up project with starter files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './styles/index.css';
+import registerServiceWorker from './registerServiceWorker';
+import productApp from './reducers/reducer'
+import App from './components/App'
+
+const store = createStore(
+  productApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
